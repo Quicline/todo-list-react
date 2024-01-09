9e0396c49c33b61ae27720038084b82720b3f870
Quicline <armando17171771@gmail.com>
2024-01-08 21:39:31 -0500

    finished todo-list-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+  
+  function handleSubmit(e) {
+    e.preventDefault();
 
+    setTodos(currentTodo => {
+      return [
+        ...currentTodo, 
+        {id: crypto.randomUUID(), title: newItem, completed:false}
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodo => {
+      return currentTodo.map( todo =>{
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+  function deleteT(id) {
+    setTodos(currentTodo => {
+      return currentTodo.filter(todo => todo.id !== id)
+    })
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input value={newItem} onChange={v=> setNewItem(v.target.value)} type="text" id="item" />
       </div>
-      <h1>Vite + Reactjs</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <button className="btn">Add Item</button>
+    </form>
+    <h1 className="header">TODO List</h1>
+    <ul className="list">
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} onChange={ v => toggleTodo(todo.id, v.target.checked)} />
+              {todo.title}
+            </label>
+            <button onClick={()=>deleteT(todo.id)} className="btn btn-danger">DELETE</button>
+          </li>
+        );
+      })}
+      
+    </ul>
+  </>
+)}
 
-export default App
+export default App;
